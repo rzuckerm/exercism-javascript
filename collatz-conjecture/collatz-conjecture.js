ccdea78e36a06853ae638e198ac27371961e36a8
rzuckerm <ron.zuckerman@adtran.com>
2024-08-04 09:06:32 -0500

    Add Collatz Conjecture in Javascript

---
+++ collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @returns {number}
+ */
+export const steps = (n) => {
+  if (n < 1) {
+    throw new Error('Only positive numbers are allowed');
+  }
+
+  for (var k = 0; n != 1; k++, n = (n % 2 == 0) ? n / 2 : 3 * n + 1);
+  return k;
+};
