32d0e8eb17a88e1e0ce51a9157f2c9598fdc4dbd
rzuckerm <ron.zuckerman@adtran.com>
2024-08-10 10:32:51 -0500

    Add High Scores in Javascript

---
+++ high-scores/high-scores.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} scores 
+ */
+export function HighScores(scores) {
+    this.scores = [...scores];
+    this.latest = scores[scores.length - 1];
+    this.personalBest = Math.max(...scores);
+    this.personalTopThree = scores.sort((a, b) => b - a).slice(0, 3);
+}
