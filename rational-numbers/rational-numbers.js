75b723f8d6336d5621bbd1580e31f6dbce69972f
rzuckerm <ron.zuckerman@adtran.com>
2024-08-18 18:02:41 -0500

    Add Rational Number in Javascript

---
+++ rational-numbers/rational-numbers.js
@@ -0,0 +1,57 @@
+export class Rational {
+  /**
+   * @param {number} n
+   * @param {number} d
+   */
+  constructor(n, d) {
+    let g = gcd(n, d);
+    this.n = (n == 0) ? 0 : Math.floor(Math.sign(d) * n / g);
+    this.d = Math.floor(Math.abs(d) / g);
+  }
+
+  /**
+   * @param {Rational} other
+   * @returns {Rational}
+   */
+  add(other) { return new Rational(this.n * other.d + this.d * other.n, this.d * other.d); }
+
+  /**
+   * @param {Rational} other
+   * @returns {Rational}
+   */
+  sub(other) { return new Rational(this.n * other.d - this.d * other.n, this.d * other.d); }
+
+  /**
+   * @param {Rational} other
+   * @returns {Rational}
+   */
+  mul(other) { return new Rational(this.n * other.n, this.d * other.d); }
+
+  /**
+   * @param {Rational} other
+   * @returns {Rational}
+   */
+  div(other) { return new Rational(this.n * other.d, this.d * other.n); }
+
+  abs() { return new Rational(Math.abs(this.n), this.d); }
+
+  /**
+   * @param {number} a
+   * @returns {Rational} 
+   */
+  exprational(a) { return new Rational(this.n ** a, this.d ** a); }
+
+  /**
+   * @param {number} a
+   * @returns {float} 
+   */
+  expreal(a) { return (a ** (1 / this.d)) ** this.n; }
+
+  reduce() { return this; }
+}
+
+const gcd = (a, b) => {
+  [a, b] = [Math.abs(a), Math.abs(b)];
+  while (b) { [a, b] = [b, a % b]; }
+  return a;
+}
