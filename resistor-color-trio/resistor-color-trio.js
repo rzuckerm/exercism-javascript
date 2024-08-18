9d6cf874eb535f15d9f51d4b7c266a6cc78b95b3
rzuckerm <ron.zuckerman@adtran.com>
2024-08-18 09:22:57 -0500

    Improve Resistor Color Trio in Javascript

--- resistor-color-trio/resistor-color-trio.js
+++ resistor-color-trio/resistor-color-trio.js
@@ -1,14 +1,10 @@
-export class ResistorColorTrio {
-  static COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
-
-  /**
-   * @param {string[]} colors
-   * @throws {error}
-   */
-  constructor(colors) {
-    if (!colors.every((c) => ResistorColorTrio.COLORS.includes(c))) { throw new Error('invalid color'); }
-    let v = colors.map((c) => ResistorColorTrio.COLORS.indexOf(c));
-    let units = ['', 'kilo', 'mega', 'giga'][Math.floor((v[2] + 1) / 3)] + 'ohms';
-    this.label = `Resistor value: ${(v[0] * 10 + v[1]) * 10 ** (((v[2] + 1) % 3) - 1)} ${units}`;
-  }
+/**
+ * @param {string[]} colors
+ * @throws {error}
+ */
+export function ResistorColorTrio(colors) {
+  const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+  let v = colors.map((c) => COLORS.includes(c) ? COLORS.indexOf(c) : (() => { throw Error('invalid color') })());
+  let units = ['', 'kilo', 'mega', 'giga'][Math.floor((v[2] + 1) / 3)] + 'ohms';
+  this.label = `Resistor value: ${(v[0] * 10 + v[1]) * 10 ** (((v[2] + 1) % 3) - 1)} ${units}`;
 }
