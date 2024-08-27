3442ef725865cc2df68d4d29f5e8554ffb67560b
rzuckerm <ron.zuckerman@adtran.com>
2024-08-26 20:23:36 -0500

    Add Kindergarten Garden in Javascript

---
+++ kindergarten-garden/kindergarten-garden.js
@@ -0,0 +1,22 @@
+const PUPILS = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry'];
+const PLANTS = { G: 'grass', V: 'violets', R: 'radishes', C: 'clover' };
+
+export class Garden {
+  /**
+   * @param {string} diagram 
+   * @param {string[]} students 
+   */
+  constructor(diagram, students = PUPILS) {
+    this.diagram = diagram.split('\n').map((row) => row.split('')).map((row) => row.map((p) => PLANTS[p]));
+    this.students = [...students].sort();
+  }
+
+  /**
+   * @param {string} student
+   * @returns {string[]}
+   */
+  plants(student) {
+    let k = this.students.indexOf(student) * 2;
+    return [...this.diagram[0].slice(k, k + 2), ...this.diagram[1].slice(k, k + 2)];
+  }
+}
