b33bd5162948bba49654f7acc89e8d9d06e5cce5
rzuckerm <ron.zuckerman@adtran.com>
2025-06-11 17:47:44 -0500

    Update tests for Collatz Conjecture in JavaScript

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -4,7 +4,7 @@
  */
 export const steps = (n) => {
   if (n < 1) {
-    throw new Error('Only positive numbers are allowed');
+    throw new Error('Only positive integers are allowed');
   }
 
   for (var k = 0; n != 1; k++, n = (n % 2 == 0) ? n / 2 : 3 * n + 1);
