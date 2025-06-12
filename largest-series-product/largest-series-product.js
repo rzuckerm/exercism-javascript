c0387ec1fc6d0b6ce7644ba07962289765fb371f
rzuckerm <ron.zuckerman@adtran.com>
2025-06-12 17:47:40 -0500

    Update tests for Largest Series Product in JavaScript

--- largest-series-product/largest-series-product.js
+++ largest-series-product/largest-series-product.js
@@ -4,9 +4,9 @@
  * @returns {number}
  */
 export const largestProduct = (digits, span) => {
-  throwIf(span < 1, 'Span must be greater than zero');
-  throwIf(span > digits.length, 'Span must be smaller than string length');
-  throwIf(/\D/.test(digits), 'Digits input must only contain digits');
+  throwIf(span < 1, 'span must not be negative');
+  throwIf(span > digits.length, 'span must not exceed string length');
+  throwIf(/\D/.test(digits), 'digits input must only contain digits');
   digits = [...digits].map(Number);
   return Math.max(...digits.slice(0, digits.length - span + 1)
     .map((_, n) => digits.slice(n, n + span).reduce((acc, v) => acc * v, 1)));
