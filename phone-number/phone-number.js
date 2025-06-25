79673dbdb813ad4af39b03ceabe5f38807e954df
rzuckerm <ron.zuckerman@adtran.com>
2025-06-25 17:29:02 -0500

    Update tests for Phone Number in JavaScript

--- phone-number/phone-number.js
+++ phone-number/phone-number.js
@@ -5,9 +5,9 @@
 export const clean = (input) => {
   let output = input.replace(/[\s().+-]/g, '');
   output = (output.startsWith('1') && output.length == 11) ? output.slice(1) : output;
-  throwIf(output.length < 10, 'Incorrect number of digits');
+  throwIf(output.length < 10, 'Must not be fewer than 10 digits');
   throwIf(output.length == 11, '11 digits must start with 1');
-  throwIf(output.length > 11, 'More than 11 digits');
+  throwIf(output.length > 11, 'Must not be greater than 11 digits');
   throwIf(/[A-Za-z]/.test(output), 'Letters not permitted');
   throwIf(/\D/.test(output), 'Punctuations not permitted');
   throwIf(/^[01]/.test(output), `Area code cannot start with ${(output[0] == '0') ? 'zero' : 'one'}`);
