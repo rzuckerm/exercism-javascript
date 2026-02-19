f010e6f1c838990dab35d93d8b3832652dabab04
rzuckerm <rzdulc@gmail.com>
2026-02-18 19:11:31 -0600

    Add Prism in JavaScript

---
+++ prism/prism.js
@@ -0,0 +1,28 @@
+// Use unit vector method recommended by Google AI
+const DEG_TO_RAD = Math.PI / 180;
+const TOL = 1e-6;
+
+export const findSequence = (start, prisms) => {
+  let current = {...start, id: null};
+  let ids = [];
+  do {
+    const [unitX, unitY] = [Math.cos(current.angle * DEG_TO_RAD), Math.sin(current.angle * DEG_TO_RAD)];
+    var selectedPrism = null;
+    var minDist = Infinity;
+    for (const prism of prisms) {
+      const [dx, dy] = [prism.x - current.x, prism.y - current.y];
+      let dist = Math.sqrt(dx * dx + dy * dy);
+      let dot = dx * unitX + dy * unitY;
+      if (dist > 0.0 && dist < minDist && dot >= dist * (1 - TOL) && dot <= dist * (1 + TOL)) {
+        [selectedPrism, minDist] = [prism, dist];
+      }
+    }
+
+    if (selectedPrism) {
+      ids.push(selectedPrism.id);
+      current = {...selectedPrism, angle: (current.angle + selectedPrism.angle) % 360};
+    }
+  } while (selectedPrism);
+
+  return ids;
+};
