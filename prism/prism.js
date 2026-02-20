562556e68720a016d4e28b745861b90e7af6f5d9
rzuckerm <rzdulc@gmail.com>
2026-02-20 17:14:22 -0600

    Remove 2 more lines from Prism in JavaScript

--- prism/prism.js
+++ prism/prism.js
@@ -3,22 +3,19 @@ const DEG_TO_RAD = Math.PI / 180;
 const TOL = 1e-6;
 
 export const findSequence = (start, prisms) => {
-  let current = {...start, id: null};
+  let current = { ...start, id: null };
   let ids = [];
   while (true) {
     const [unitX, unitY] = [Math.cos(current.angle * DEG_TO_RAD), Math.sin(current.angle * DEG_TO_RAD)];
-    let selectedPrism = null;
-    let minDist = Infinity;
-    for (const prism of prisms) {
-      const [dx, dy] = [prism.x - current.x, prism.y - current.y];
-      let dist = Math.sqrt(dx * dx + dy * dy);
-      let dot = (dist > 0) ? (dx * unitX + dy * unitY) / dist : 1;
-      if (dist > 0.0 && dist < minDist && Math.abs(dot - 1) <= TOL) { [selectedPrism, minDist] = [prism, dist]; }
-    }
-
+    const selectedPrism = prisms.filter((p) => p.x != current.x || p.y != current.y).reduce(
+      ([bestP, minDist], p) => {
+        const [dx, dy] = [p.x - current.x, p.y - current.y];
+        const dist = Math.hypot(dx, dy);
+        return ((dist < minDist) && Math.abs((dx * unitX + dy * unitY) / dist - 1) <= TOL) ? [p, dist] : [bestP, minDist];
+      }, [null, Infinity])[0];
     if (!selectedPrism) { break; }
     ids.push(selectedPrism.id);
-    current = {...selectedPrism, angle: (current.angle + selectedPrism.angle) % 360};
+    current = { ...selectedPrism, angle: (current.angle + selectedPrism.angle) % 360 };
   }
 
   return ids;
